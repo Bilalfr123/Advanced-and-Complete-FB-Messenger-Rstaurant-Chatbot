db766cc41e8e1ab2afd0c585249ada6c4858170e
unknown <darklight1062@gmail.com>
2022-03-10 11:05:35 -0800

    updat

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -313,6 +313,19 @@ let sendMessageAskingPhoneNumber = (sender_psid) => {
             ]
         }
     }
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
     };
 let askQuantity = async(sender_psid)=>{
     let request_body = {
