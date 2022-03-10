7770e3ff39d32de69660dc3db13bdb931e5444db
unknown <darklight1062@gmail.com>
2022-03-10 10:01:17 -0800

    updat

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -80,7 +80,7 @@ function firstTrait(nlp, name) {
 // Handles messages events
 let handleMessage =async (sender_psid, message) => {
     //checking if the message is quick reply with options
-    if(message & message.quick_reply && message.quick_reply.payload){
+    if(message && message.quick_reply && message.quick_reply.payload){
    if ( message.quick_reply.payload === 'SMALL') {
             await chatBotService.askPhoneNumber(sender_psid);
         
