918b6a6643e78abc3acdd9a6c29c50292d136537
unknown <darklight1062@gmail.com>
2022-03-12 10:56:34 -0800

    updat

--- src/routes/web.js
+++ src/routes/web.js
@@ -7,8 +7,8 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homepageController.getHomepage);
     router.get("/webhook", homepageController.getWebhook);
-    router.get("/profile", homepageController.getFacebookUserProfile);
-    router.post("/set-up-user-fb-profile", homepageController.setUpUserFacebookProfile);
+    // router.get("/profile", homepageController.getFacebookUserProfile);
+    // router.post("/set-up-user-fb-profile", homepageController.setUpUserFacebookProfile);
     router.post("/webhook", homepageController.postWebhook);
     // router.post("/setup", homepageController.handleSetupInfor); //persistent menu
     return app.use("/", router);
